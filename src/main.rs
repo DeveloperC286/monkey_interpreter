8604349c58c1eb642ee4465c1ac80ad9a75c9217
C <DeveloperC@protonmail.com>
2020-04-03 19:26:39 +0000

    Added interactive REPL loop by default.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+extern crate pretty_env_logger;
+
+#[macro_use]
+extern crate log;
+
 #[macro_use]
 extern crate lazy_static;
 
 mod lexer;
+mod repl;
 mod token;
 
-fn main() {}
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "rust-monkey-interpreter",
+    about = "Monkey interpreter written in Rust."
+)]
+struct Args {}
+
+fn main() {
+    pretty_env_logger::init();
+    let args = Args::from_args();
+    repl::repl();
+}
---
+++ src/repl/mod.rs
@@ -0,0 +1,27 @@
+use super::lexer::Lexer;
+use super::token::TokenType;
+use std::io::{self};
+
+pub fn repl() {
+    loop {
+        let input = read();
+        let mut lexer = Lexer::new(input);
+        let mut token = lexer.get_next_token();
+
+        while token.token_type != TokenType::EOF {
+            info!("> {:?}", token);
+            token = lexer.get_next_token();
+        }
+    }
+}
+
+fn read() -> String {
+    let mut buffer = String::new();
+
+    match io::stdin().read_line(&mut buffer) {
+        Ok(_n) => {}
+        Err(error) => error!("Error reading user input: {}", error),
+    }
+
+    return buffer;
+}
