4d2bc637010f418973bb0fd6dea75900afbd784d
C <DeveloperC@protonmail.com>
2020-04-16 00:55:12 +0000

    Adding Expression enums.
    Adding in the Expression enum container and the expression precedence
    enum.

--- src/syntax_analysis/abstract_syntax_tree/syntax_tree_node/mod.rs
+++ src/syntax_analysis/abstract_syntax_tree/syntax_tree_node/mod.rs
@@ -9,4 +9,26 @@ pub enum SyntaxTreeNode {
     ReturnStatement {
         return_token: Token,
     },
+    ExpressionStatement {
+        expression_token: Token,
+        expression: Expression,
+    },
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Expression {
+    IDENTIFIER { identifier_token: Token },
+    INTEGER { integer_token: Token },
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[allow(non_camel_case_types)]
+pub enum ExpressionPrecedence {
+    LOWEST,
+    EQUALS,
+    LESSER_OR_GREATER,
+    PLUS,
+    MULTIPLY,
+    PREFIX,
+    FUNCTION_CALL,
 }
