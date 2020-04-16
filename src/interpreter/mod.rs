ef70737209c6802dc90bc3e783f6dd4afa6a30e4
C <DeveloperC@protonmail.com>
2020-04-16 18:26:07 +0000

    Added logging + REPL syntax parsing.
    The REPL loop now passes the lexial parser token stream output into the
    syntax parser and prints the token stream and abstract syntax tree.

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,19 +1,36 @@
-use super::lexical_analysis::token::TokenType;
+use super::lexical_analysis::token::{Token, TokenType};
 use super::lexical_analysis::LexicalAnalysis;
+use super::syntax_analysis::abstract_syntax_tree::AbstractSyntaxTree;
+use super::syntax_analysis::SyntaxAnalysis;
 
 use std::io::{self};
 
 pub fn repl() {
     loop {
-        let input = read();
-        let mut lexical_analysis = LexicalAnalysis::new(input);
-        let mut token = lexical_analysis.get_next_token();
-
-        while token.token_type != TokenType::EOF {
-            println!("> {:?}", token);
-            token = lexical_analysis.get_next_token();
-        }
+        let tokens = get_tokens(read());
+        println!("{:?}", tokens);
+        let ast = get_abstract_syntax_tree(tokens);
+        println!("{:?}", ast);
     }
+}
+
+fn get_abstract_syntax_tree(tokens: Vec<Token>) -> AbstractSyntaxTree {
+    let mut syntax_analysis = SyntaxAnalysis::new(tokens);
+    return syntax_analysis.parse();
+}
+
+fn get_tokens(input: String) -> Vec<Token> {
+    let mut lexical_analysis = LexicalAnalysis::new(input);
+    let mut token = lexical_analysis.get_next_token();
+    let mut tokens: Vec<Token> = vec![];
+
+    while token.token_type != TokenType::EOF {
+        tokens.push(token);
+        token = lexical_analysis.get_next_token();
+    }
+    tokens.push(token);
+
+    return tokens;
 }
 
 fn read() -> String {
--- src/syntax_analysis/mod.rs
+++ src/syntax_analysis/mod.rs
@@ -1,6 +1,6 @@
 use super::lexical_analysis::token::{Token, TokenType};
 
-mod abstract_syntax_tree;
+pub mod abstract_syntax_tree;
 use abstract_syntax_tree::syntax_tree_node::{Expression, ExpressionPrecedence, SyntaxTreeNode};
 use abstract_syntax_tree::AbstractSyntaxTree;
 
@@ -21,6 +21,10 @@ impl SyntaxAnalysis {
         let mut program: Vec<SyntaxTreeNode> = vec![];
 
         loop {
+            debug!(
+                "Syntax parsing the Token '{:?}' at index {}.",
+                self.tokenized_code[self.current_token_index], self.current_token_index
+            );
             match self.tokenized_code[self.current_token_index].token_type {
                 TokenType::EOF => break,
                 _ => {
@@ -45,11 +49,13 @@ impl SyntaxAnalysis {
     }
 
     fn parse_expression_statement(&mut self) -> Option<SyntaxTreeNode> {
+        debug!("Parsing an expression statement.");
         let initial_expression_token = self.tokenized_code[self.current_token_index].clone();
 
         let expression_option = self.parse_expression(ExpressionPrecedence::LOWEST);
 
         if self.tokenized_code[self.current_token_index].token_type == TokenType::SEMI_COLON {
+            trace!("Jumping over semi colon.");
             self.increment_token_index();
         }
 
@@ -68,51 +74,66 @@ impl SyntaxAnalysis {
         &mut self,
         expression_precedence: ExpressionPrecedence,
     ) -> Option<Expression> {
+        debug!("Parsing an expression.");
         let token = self.tokenized_code[self.current_token_index].clone();
         self.increment_token_index();
 
         match token.token_type {
             TokenType::IDENTIFIER => {
+                debug!("Returning an identifier expression.");
                 return Some(Expression::IDENTIFIER {
                     expression_precedence,
                     identifier_token: token,
                 });
             }
             TokenType::INTEGER => {
+                debug!("Returning an integer expression.");
                 return Some(Expression::INTEGER {
                     expression_precedence,
                     integer_token: token,
                 });
             }
             _ => {
+                debug!("Unable to parse expression.");
                 return None;
             }
         }
     }
 
     fn parse_return_statement(&mut self) -> Option<SyntaxTreeNode> {
+        debug!("Parsing a return statement.");
         let return_token = self.tokenized_code[self.current_token_index].clone();
 
         //TODO handle expression.
 
         self.increment_token_index();
+
         loop {
             match self.tokenized_code[self.current_token_index].token_type {
-                TokenType::SEMI_COLON => break,
-                _ => {}
+                TokenType::SEMI_COLON => {
+                    self.increment_token_index();
+                    break;
+                }
+                _ => {
+                    self.increment_token_index();
+                }
             }
-            self.increment_token_index();
         }
 
         return Some(SyntaxTreeNode::RETURN_STATEMENT { return_token });
     }
 
     fn parse_let_statement(&mut self) -> Option<SyntaxTreeNode> {
+        debug!("Parsing a let statement.");
         let let_token = self.tokenized_code[self.current_token_index].clone();
 
         self.increment_token_index();
 
         if self.tokenized_code[self.current_token_index].token_type != TokenType::IDENTIFIER {
+            error!(
+                "No TokenType::IDENTIFIER at '{}' for the let statement.",
+                self.current_token_index
+            );
             //TODO add error message about no IDENTIFIER.
             return None;
         }
@@ -121,6 +142,10 @@ impl SyntaxAnalysis {
         self.increment_token_index();
 
         if self.tokenized_code[self.current_token_index].token_type != TokenType::ASSIGN {
+            error!(
+                "No TokenType::ASSIGN at '{}' for the let statement.",
+                self.current_token_index
+            );
             //TODO add error message about no ASSIGN.
             return None;
         }
@@ -128,6 +153,7 @@ impl SyntaxAnalysis {
         //TODO handle expression.
 
         self.increment_token_index();
+
         loop {
             match self.tokenized_code[self.current_token_index].token_type {
                 TokenType::SEMI_COLON => {
@@ -147,6 +173,7 @@ impl SyntaxAnalysis {
     }
 
     fn increment_token_index(&mut self) {
+        trace!("Incrementing the token index.");
         self.current_token_index += 1;
     }
 }
