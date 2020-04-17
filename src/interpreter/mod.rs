baa65aec42805eacec7057e536728d330f1f41a1
C <DeveloperC@protonmail.com>
2020-04-17 16:13:20 +0000

    Interpreter updated formatting.
    Added package name and version to stdout at start of interpreter
    starting up to keep the user informed.
    
    Added ' >>> ' to the beginning of stdin reading, so they know they can
    now enter text and that it is not just waiting.

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -3,9 +3,17 @@ use super::lexical_analysis::LexicalAnalysis;
 use super::syntax_analysis::abstract_syntax_tree::AbstractSyntaxTree;
 use super::syntax_analysis::SyntaxAnalysis;
 
-use std::io::{self};
+use std::io::{stdin, stdout, Write};
+
+const NAME: Option<&'static str> = option_env!("CARGO_PKG_NAME");
+const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
 pub fn repl() {
+    println!(
+        "{} {}",
+        NAME.unwrap_or("unknown"),
+        VERSION.unwrap_or("unknown")
+    );
     loop {
         let tokens = get_tokens(read());
         println!("{:?}", tokens);
@@ -36,7 +44,10 @@ fn get_tokens(input: String) -> Vec<Token> {
 fn read() -> String {
     let mut buffer = String::new();
 
-    match io::stdin().read_line(&mut buffer) {
+    print!(" >>> ");
+    let _ = stdout().flush();
+
+    match stdin().read_line(&mut buffer) {
         Ok(_n) => {}
         Err(error) => error!("Error reading user input: {}", error),
     }
