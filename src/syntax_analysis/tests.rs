f28c8c1fe359250d6c21a92fb7bd753fab5f9dc6
C <DeveloperC@protonmail.com>
2020-05-23 19:45:45 +0000

    test: additional functions parameters tests

--- src/syntax_analysis/tests.rs
+++ src/syntax_analysis/tests.rs
@@ -25,6 +25,29 @@ use rstest::rstest;
         ],
         "test_syntax_analysis_for_function_expression_case1"
     ),
+    case(
+        vec![
+            Token{token_type: TokenType::FUNCTION, literal: "fn".to_string()},
+            Token{token_type: TokenType::OPENING_ROUND_BRACKET, literal: "(".to_string()},
+            Token{token_type: TokenType::CLOSING_ROUND_BRACKET, literal: ")".to_string()},
+            Token{token_type: TokenType::OPENING_CURLY_BRACKET, literal: "{".to_string()},
+            Token{token_type: TokenType::CLOSING_CURLY_BRACKET, literal: "}".to_string()},
+            Token{token_type: TokenType::EOF, literal: "".to_string()},
+        ],
+        "test_syntax_analysis_for_function_expression_case2"
+    ),
+    case(
+        vec![
+            Token{token_type: TokenType::FUNCTION, literal: "fn".to_string()},
+            Token{token_type: TokenType::OPENING_ROUND_BRACKET, literal: "(".to_string()},
+            Token{token_type: TokenType::IDENTIFIER, literal: "x".to_string()},
+            Token{token_type: TokenType::CLOSING_ROUND_BRACKET, literal: ")".to_string()},
+            Token{token_type: TokenType::OPENING_CURLY_BRACKET, literal: "{".to_string()},
+            Token{token_type: TokenType::CLOSING_CURLY_BRACKET, literal: "}".to_string()},
+            Token{token_type: TokenType::EOF, literal: "".to_string()},
+        ],
+        "test_syntax_analysis_for_function_expression_case3"
+    ),
 )]
 fn test_syntax_analysis_for_function_expression(tokens: Vec<Token>, snapshot_name: &str) {
     assert_expected_returned_abstract_syntax_tree(tokens, snapshot_name);
