97e4b697d83a37b6956d1a1c0927283a88b2e159
C <DeveloperC@protonmail.com>
2020-08-12 00:17:09 +0000

    test: additional return statement tests

--- src/evaluator/return_statement/tests.rs
+++ src/evaluator/return_statement/tests.rs
@@ -8,6 +8,18 @@ use rstest::rstest;
         "return 10;".to_string(),
         "test_evaluator_return_nodes_case1"
     ),
+    case(
+        "return TRUE; 9;".to_string(),
+        "test_evaluator_return_nodes_case2"
+    ),
+    case(
+        "return 3 * 4; FALSE;".to_string(),
+        "test_evaluator_return_nodes_case3"
+    ),
+    case(
+        "13; return 8 / 2; 0;".to_string(),
+        "test_evaluator_return_nodes_case4"
+    ),
 )]
 fn test_evaluator_return_nodes(code: String, snapshot_name: &str) {
     assert_expected_returned_object!(code, snapshot_name);
