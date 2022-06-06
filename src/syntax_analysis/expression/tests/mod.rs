e69d966e40907b16431577ae9cab9cd40073a44b
DeveloperC <DeveloperC@protonmail.com>
2022-06-06 18:41:11 +0100

    refactor: invalid boolean integer expression global tests

--- src/syntax_analysis/expression/tests/mod.rs
+++ src/syntax_analysis/expression/tests/mod.rs
@@ -3,15 +3,6 @@ use rstest::rstest;
 #[rstest(
     code,
     snapshot_name,
-    case("-;", "test_syntax_analysis_for_prefix_expression_syntax_errors_case2")
-)]
-fn test_syntax_analysis_for_prefix_expression_syntax_errors(code: &str, snapshot_name: &str) {
-    assert_expected_returned_abstract_syntax_tree!(code, snapshot_name)
-}
-
-#[rstest(
-    code,
-    snapshot_name,
     case("!15", "test_syntax_analysis_for_prefix_expressions_case1")
 )]
 fn test_syntax_analysis_for_prefix_expressions(code: &str, snapshot_name: &str) {
--- src/tests/syntax_analysis_error/mod.rs
+++ src/tests/syntax_analysis_error/mod.rs
@@ -4,4 +4,5 @@ use super::*;
 mod macros;
 
 mod invalid_boolean_prefix_expression;
+mod invalid_integer_prefix_expression;
 mod invalid_let_statement;
