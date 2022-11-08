dbd72b87f370cd37eb7ebcde881546db1ee7ab4e
DeveloperC <DeveloperC@protonmail.com>
2022-11-08 21:53:45 +0000

    refactor: simplifying edge case testing naming

--- src/tests/edge_cases/mod.rs
+++ src/tests/edge_cases/mod.rs
@@ -1,25 +1,25 @@
 use super::*;
 
 #[template]
-#[rstest(code, snapshot_name, case("", "edge_cases_case_1"))]
-fn edge_cases_cases(code: &str, snapshot_name: &str) {}
+#[rstest(code, snapshot_name, case("", "edge_case_1"))]
+fn edge_cases(code: &str, snapshot_name: &str) {}
 
-#[apply(edge_cases_cases)]
+#[apply(edge_cases)]
 fn test_edge_cases_lexical_analysis(code: &str, snapshot_name: &str) {
     assert_lexical_analysis!(code, snapshot_name);
 }
 
-#[apply(edge_cases_cases)]
+#[apply(edge_cases)]
 fn test_edge_cases_syntax_analysis(code: &str, snapshot_name: &str) {
     assert_syntax_analysis!(code, snapshot_name);
 }
 
-#[apply(edge_cases_cases)]
+#[apply(edge_cases)]
 fn test_edge_cases_evaluator(code: &str, snapshot_name: &str) {
     assert_evaluator!(code, snapshot_name);
 }
 
-#[apply(edge_cases_cases)]
+#[apply(edge_cases)]
 fn test_edge_cases_environment(code: &str, snapshot_name: &str) {
     assert_environment!(code, snapshot_name);
 }
