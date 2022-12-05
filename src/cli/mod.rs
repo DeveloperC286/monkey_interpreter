b9165d7cdddc996d0d7a7ea2e0db4b1b743ec613
DeveloperC <DeveloperC@protonmail.com>
2022-12-05 20:27:09 +0000

    refactor: removing unused structopt CLI parsing

--- src/cli/mod.rs
+++
@@ -1,8 +0,0 @@
-use structopt::StructOpt;
-
-#[derive(Debug, StructOpt)]
-#[structopt(
-    name = "Monkey Interpreter",
-    about = "Implementation of an interpreter for the Monkey language written in Rust, currently under active development."
-)]
-pub(crate) struct Arguments {}
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@
 extern crate log;
 extern crate pretty_env_logger;
 
-use structopt::StructOpt;
-
-mod cli;
 mod evaluator;
 mod interpreter;
 mod lexical_analysis;
@@ -15,7 +12,6 @@ mod syntax_analysis;
 
 fn main() {
     pretty_env_logger::init();
-    let _arguments = cli::Arguments::from_args();
     interpreter::repl();
 }
 
