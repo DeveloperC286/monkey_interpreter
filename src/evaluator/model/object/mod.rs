1bcfbe6804dfbc10930a1978350414fd6bde642e
DeveloperC <DeveloperC@protonmail.com>
2026-09-05 01:58:15 +0100

    feat: Display trait for Object enum (#378)

--- src/evaluator/model/object/mod.rs
+++ src/evaluator/model/object/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::syntax_analysis::model::syntax_tree_node::Block;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -18,4 +20,20 @@ pub(crate) enum Object {
         parameters: Vec<String>,
         block: Block,
     },
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Object::Return { value } => write!(f, "{value}"),
+            Object::Integer { value } => write!(f, "{value}"),
+            Object::String { value } => write!(f, "{value}"),
+            Object::True => write!(f, "true"),
+            Object::False => write!(f, "false"),
+            Object::Null => write!(f, "null"),
+            Object::Function { parameters, .. } => {
+                write!(f, "fn({}) {{ ... }}", parameters.join(", "))
+            }
+        }
+    }
 }
